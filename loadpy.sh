fc75c29df4b8d71420264ecb67618950d489cf98
wz5mz <wz5mz@virginia.edu>
2019-04-24 16:30:45 -0700

    Update loadpy.sh
    On Android devices, load python scripts into the Termux directory ~/.termux/tasker for use with Tasker.
    
    $1 = github directory (NO slashes)
    $2 = filename with extension (NO slashes)

--- loadpy.sh
+++ loadpy.sh
@@ -1,4 +1,26 @@
 #! /data/data/com.termux/files/usr/bin/bash/sh
 
-echo $1
-echo $2
+# $1 = github directory without ANY slashes
+# $2 = filename with extension (NO slashes)
+
+url=https://github.com/wz5mz/
+url+=$1
+url+=.git
+echo $url
+
+copypath=./
+copypath+=$1
+copypath+=/
+copypath+=$2
+
+path2=./
+path2+=$2
+
+cd ~/
+git clone url
+cp copypath ~/.termux/tasker/
+cd ~/.termux/tasker/
+termux-fix-shebang path2
+chmod +x path2
+cd ~/
+rm -rf $1
